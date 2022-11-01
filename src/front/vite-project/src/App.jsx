2bc588827db530515002c3879ed1b5b61f152aa5
Piotr Wojcik <p.p.wojcik@interia.pl>
2022-11-01 19:01:59 +0100

    project start:

---
+++ src/front/vite-project/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './styles/App.css'
+import Header from './components/Header'
+import MainContent from './components/MainContent'
+
+export default function App() {
+
+  return (
+    <div className="App">
+      <Header/>
+      <MainContent/>
+    </div>
+  )
+}
---
+++ src/front/vite-project/src/components/MainContent.jsx
@@ -0,0 +1,8 @@
+export default function MainContent() {
+
+    return (
+        <main>
+            <h2>Hello World!</h2>
+        </main>
+    )
+}
