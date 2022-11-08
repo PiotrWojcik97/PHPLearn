b6f04b6cbe3900d4d0770f96924effa8182d01b0
Piotr Wojcik <p.p.wojcik@interia.pl>
2022-11-08 13:35:42 +0100

    php initial post

---
+++ src/back/api/v1/read.php
@@ -0,0 +1,42 @@
+<?php
+
+header('Access-Control-Allow-Origin: *');
+header('Consent-Type: application/json');
+
+include_once '../../config/Database.php';
+include_once '../../models/Users.php';
+
+$database = new Database();
+$db = $database->connect();
+
+$result = $post->read();
+// debug in home what's the result
+
+$num_of_rows = $result->rowCount();
+
+if($num > 0)
+{
+    $users_arr = array();
+    $users_arr['data'] = array();
+
+    while($row = $result->fetch(PDO::FETCH_ASSOC))
+    {
+        extract($row);
+
+        $user_item = array(
+            'id' => $id,
+            'username' => $username,
+            'password' => $password
+        );
+
+        array_push($users_arr['data'], $user_item);
+    }
+    echo json_encode($users_arr);
+}
+else
+{
+    //no users
+    echo json_encode(
+        array('message' => "No Users Found")
+    );
+}
---
+++ src/back/models/Users.php
@@ -0,0 +1,28 @@
+<?php
+
+class Users
+{
+    private $conn;
+    private $table = 'users';
+
+    public $id;
+    public $username;
+    public $password;
+
+    public function __construct($db)
+    {
+        $this->conn = $db;
+    }
+
+    public function read()
+    {
+        $query = 
+            'SELECT u.id, u.username, u.password
+            FROM' . $this->table . 'as u';
+
+        $stmt = $this->conn->prepare($query);
+        $stmt->execute();
+
+        return $stmt;
+    }
+}
